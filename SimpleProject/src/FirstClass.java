e84cb59e0ebc2b424c8f77f44d8472fc428dc300
tmgiang <tmgiang@spsvietnam.vn>
2014-04-17 17:07:51 +0700

    Commit Simple project

---
+++ SimpleProject/src/FirstClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author tmgiang
+ *
+ */
+public class FirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Show me");
+		System.out.println("This is demo project!!!");
+
+	}
+
+}
