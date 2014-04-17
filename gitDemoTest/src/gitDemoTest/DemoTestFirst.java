df9b7ee75be2c35221d477d9ae144bd85f60b35c
tmgiang <tmgiang@spsvietnam.vn>
2014-04-17 10:15:57 +0700

    commit first project

---
+++ gitDemoTest/src/gitDemoTest/DemoTestFirst.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gitDemoTest;
+
+/**
+ * @author tmgiang
+ *
+ */
+public class DemoTestFirst {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("print tui di");
+		//
+	}
+
+}
