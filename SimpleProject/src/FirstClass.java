06b4e8a9ed5e6ee2005c0a43aad4643d2dc1f602
tmgiang <tmgiang@spsvietnam.vn>
2014-04-18 11:41:55 +0700

    Trigger again

--- SimpleProject/src/FirstClass.java
+++ SimpleProject/src/FirstClass.java
@@ -15,7 +15,7 @@ public class FirstClass {
 		// TODO Auto-generated method stub
 		System.out.println("Show me");
 		System.out.println("This is demo project!!!");
-		System.out.println("Trigger a Build 6666 !!!");
+		System.out.println("Trigger a Build Against !!!");
 		System.out.println("Now build with Poll SCM !!!");
 
 	}
